8b78bab6e740c2038ae5c91dde4c99bb14f82257
TheK0tYaRa <alod123@mail.ua>
2021-01-31 15:56:02 +0200

    Initial

---
+++ orangefox_build_interactive.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Comment blocks
+
+fresh-start_i () {
+  case $1 in
+    1)
+      echo "Config not found, creating sample config..."
+      ;;
+    2)
+      echo "Config created using local folder as root folder, execute the script again when ready."
+      ;;
+    *)
+  esac
+}
+
+
+
+# Function blocks
+
+fresh-start () {
+  if [[ $(ls | grep "orangefox_build.config") == "" ]]; then {
+    fresh-start_i 1
+    cat > orangefox_build.config << EOL
+LOCATION=$PWD
+EOL
+    fresh-start_i 2
+  } fi
+}
+
+# Main code
+
+if [[ $# -eq 0 ]]; then {
+  fresh-start # Check for existing configuration
+} fi
+
+echo "If you wish to do a manual setup for your device - input it now, othervice it will be assumed based on a connected currently phone."
+echo "If you wish to do an automated setup - connect and unlock your device now"
+mode="auto"
+read -p "Pick a mode [auto]: " tmp
+if [[ "$tmp" != "" ]]; then mode=$tmp; unset tmp ; fi
+echo $mode # Temporary
